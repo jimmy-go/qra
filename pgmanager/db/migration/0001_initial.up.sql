f00e546d098bbd2873c5df71bf2883f47834f547
Angel Del Castillo <jubu201504@gmail.com>
2016-09-16 04:36:59 -0500

    system identity generation

--- pgmanager/db/migration/0001_initial.up.sql
+++ pgmanager/db/migration/0001_initial.up.sql
@@ -1,7 +1,91 @@
 /*
-    PostgreSQL default migration for manager default example.
+    PostgreSQL default migration for QRA manager.
 */
 
-CREATE TABLE person (
-TODO;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE OR REPLACE FUNCTION update_modified_column()
+RETURNS trigger AS
+$BODY$
+BEGIN
+    NEW.updated_at = now();
+    RETURN NEW;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+COST 100;
+ALTER FUNCTION update_modified_column()
+OWNER TO postgres;
+
+CREATE TABLE identity (
+    id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+    name text,
+    password bytea,
+    created_at timestamp with time zone DEFAULT now(),
+    updated_at timestamp with time zone DEFAULT now(),
+    CONSTRAINT identity_pkey PRIMARY KEY (id)
 )
+WITH (
+    OIDS=FALSE
+);
+
+CREATE UNIQUE INDEX identity_name_idx ON identity (name);
+
+CREATE TRIGGER update_identity_updated_at
+BEFORE UPDATE
+ON identity
+FOR EACH ROW
+    EXECUTE PROCEDURE update_modified_column();
+
+-- create admin@mail.com password: admin123
+INSERT INTO identity (name,password)
+VALUES('admin@mail.com','$2a$10$YcbHZJl1z9Mo61ZWsykCAecfJfOxTHT1.mUGJhakrONOBiywP6Wu.');
+
+CREATE TABLE session (
+    id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+    identity_id uuid REFERENCES identity (id) ON DELETE RESTRICT,
+    token text,
+    expires_at timestamp with time zone,
+    created_at timestamp with time zone DEFAULT now(),
+    CONSTRAINT session_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS=FALSE
+);
+
+DROP TYPE IF EXISTS perm;
+
+-- permission catalog. TODO; add more common permissions
+CREATE TYPE PERM AS ENUM(
+'session-on','identity-create','identity-edit','identity-delete','read','write',
+'delete'
+);
+
+CREATE TABLE designation (
+    id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+    identity text,
+    permission PERM,
+    resource text,
+    issuer_id uuid REFERENCES identity (id) ON DELETE RESTRICT,
+    issuer_signature bytea,
+    expires_at timestamp with time zone,
+    created_at timestamp with time zone DEFAULT now(),
+    CONSTRAINT designation_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS=FALSE
+);
+
+CREATE INDEX designation_issuer_signature_idx ON designation (issuer_signature);
+CREATE INDEX designation_permission_idx ON designation (permission);
+CREATE INDEX designation_resource_idx ON designation (resource);
+
+CREATE TABLE trail (
+    id uuid NOT NULL DEFAULT uuid_generate_v1mc(),
+    designation_id uuid REFERENCES designation (id) ON DELETE RESTRICT,
+    created_at timestamp with time zone DEFAULT now(),
+    CONSTRAINT trail_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS=FALSE
+);
--- pgmanager/pgmanager.go
+++ pgmanager/pgmanager.go
@@ -31,18 +31,35 @@ import (
 	// import driver in main
 	// _ "github.com/lib/pq"
 
+	"crypto/hmac"
+	"crypto/rand"
+	"crypto/sha512"
+	"errors"
+	"io"
+	"log"
+	"strings"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/jimmy-go/pgwp"
 	"github.com/jimmy-go/qra"
+	"github.com/satori/go.uuid"
 )
 
+// TODO;
+
 var (
-	sessions    *Session
-	accounts    *Account
-	roles       *Role
-	permissions *Permission
+	authentication *Authenticationer
+	designation    *Designationer
 
 	// Db database for this manager.
 	Db *pgwp.Pool
+
+	errSystemNotFound       = errors.New("pgmanager: system identity not found")
+	errPermissionNotFound   = errors.New("pgmanager: permission not found")
+	errInvalidCredentials   = errors.New("pgmanager: invalid username or password")
+	errResourceNotSpecified = errors.New("pgmanager: resource not specified")
 )
 
 // Connect starts the manager.
@@ -53,86 +70,308 @@ func Connect(driver, connectURL string) error {
 		return err
 	}
 
-	// register qra default manager or panics.
-	qra.MustRegisterSessioner(sessions)
-	qra.MustRegisterAccounter(accounts)
-	qra.MustRegisterRoler(roles)
-	qra.MustRegisterPermissioner(permissions)
+	err = systemChecksum()
+	if err != nil {
+		return err
+	}
+
+	authentication = &Authenticationer{
+		DB: Db,
+	}
+	designation = &Designationer{
+		DB: Db,
+	}
+
+	qra.MustRegisterAuthentication(authentication)
+	qra.MustRegisterDesignation(designation)
 	return nil
 }
 
-// Login login.
-func (dm *Manager) Login(username, password string) error {
-	var users []string
-	err := dm.Db.Select(&users, `
-		SELECT
-			id
-		FROM person
-		LIMIT 100;
+// systemChecksum will verifies several things:
+//		user system exists.
+//		permission catalog exists.
+//		system is owner of permission catalog.
+//		system permission designation.
+//		TODO; if not manager user present generate one
+//		with custom permission designation?
+func systemChecksum() error {
+	var systemID string
+	err := Db.Get(&systemID, `
+		SELECT id FROM identity WHERE name='system';
 	`)
-	return users, err
-}
+	if err != nil {
+		log.Printf("system identity not found: creating system")
 
-// Users func
-func (dm *Manager) Users() ([]string, error) {
-	var users []string
-	err := dm.Db.Select(&users, `
-		SELECT
-			id
-		FROM person
-		LIMIT 100;
+		// TODO; generate random password.
+		randpass := make([]byte, 64)
+		_, err := rand.Read(randpass)
+		if err != nil {
+			log.Printf("can't  create system identity : err [%s]", err)
+			return errSystemNotFound
+		}
+
+		sysPassHash, err := bcrypt.GenerateFromPassword(randpass, bcrypt.DefaultCost)
+		if err != nil {
+			log.Printf("can't  create system identity : err [%s]", err)
+			return errSystemNotFound
+		}
+
+		err = Db.Get(&systemID, `
+			INSERT INTO identity
+			(name,password)
+			VALUES($1,$2)
+			RETURNING id;
+		`, "system", sysPassHash)
+		if err != nil {
+			log.Printf("can't  create system identity : err [%s]", err)
+			return errSystemNotFound
+		}
+
+		mac := hmac.New(sha512.New, randpass)
+		mac.Write(randpass)
+		signature := mac.Sum(nil)
+
+		// create permission catalog.
+
+		// permission for 2 years.
+		expiresAt := time.Now().UTC().Add(24 * time.Hour * 365 * 2)
+
+		var perID string
+		err = Db.Get(&perID, `
+			INSERT INTO designation
+			(identity,permission,resource,issuer_id,issuer_signature,expires_at)
+			VALUES ($1,$2,$3,$4,$5,$6)
+			RETURNING id;
+			`, "system", "session-on", "terminal", systemID, signature, expiresAt)
+		if err != nil {
+			log.Printf("can't  create system identity : err [%s]", err)
+			return errSystemNotFound
+		}
+
+		log.Printf("SYSTEM IDENTITY PASSWORD: %x", randpass)
+	}
+	var c int
+	err = Db.Get(&c, `
+		SELECT count(id) FROM designation;
 	`)
-	return users, err
+	if err != nil || c < 1 {
+		return errPermissionNotFound
+	}
+	return nil
 }
 
-// Roles func
-func (dm *Manager) Roles() []string {
-	var roles []string
-	err := dm.Db.Select(&roles, `
-		SELECT
-			id
-		FROM role
-		LIMIT 100;
-	`)
-	return roles, err
+// Authenticationer struct
+type Authenticationer struct {
+	DB *pgwp.Pool
 }
 
-// RolesByUser func
-func (dm *Manager) RolesByUser(ID string) []string {
-	var roles []string
-	err := dm.Db.Select(&roles, `
-		SELECT
-			id
-		FROM role
-		WHERE user_id=$1
-		LIMIT 100;
-	`, ID)
-	return roles, err
+// IDPass for identity retrieve.
+type IDPass struct {
+	ID   string `db:"id"`
+	Pass []byte `db:"password"`
 }
 
-// Permissions func
-func (dm *Manager) Permissions() []string {
-	var pers []string
-	err := dm.Db.Select(&pers, `
-		SELECT
-			id
-		FROM permission
-		LIMIT 100;
-	`)
-	return pers, err
+// Authenticate makes login for Identity.
+func (s *Authenticationer) Authenticate(ctx qra.Identity, password string, dst interface{}) error {
+	username := ctx.Me()
+	log.Printf("Authenticate : username [%s] password [%s]", username, password)
 
+	var iden IDPass
+	err := s.DB.Get(&iden, `
+		SELECT id,password FROM identity WHERE name=$1;
+	`, username)
+	if err != nil {
+		log.Printf("Authenticate : get user : err [%s]", err)
+		return err
+	}
+	if len(iden.Pass) < 1 {
+		return errInvalidCredentials
+	}
+
+	err = bcrypt.CompareHashAndPassword(iden.Pass, []byte(password))
+	if err != nil {
+		log.Printf("Authenticate : compare password : err [%s]", err)
+		return errInvalidCredentials
+	}
+
+	expiresAt := time.Now().UTC().Add(24 * time.Hour * 30 * 3)
+	token := uuid.NewV4().String() + "." + uuid.NewV4().String()
+
+	err = s.DB.Get(dst, `
+		INSERT INTO session (identity_id,token,expires_at)
+		VALUES ($1,$2,$3)
+		RETURNING token;
+	`, iden.ID, token, expiresAt)
+	if err != nil {
+		log.Printf("Authenticate : get user : err [%s]", err)
+		return err
+	}
+
+	return nil
 }
 
-// PermissionsByUser func
-func (dm *Manager) PermissionsByUser(ID string) []string {
-	var pers []string
-	err := dm.Db.Select(&pers, `
-		SELECT
-			id
-		FROM permission
-		WHERE user_id=$1
-		LIMIT 100;
-	`, ID)
-	return pers, err
+// Close deletes current session of Identity.
+func (s *Authenticationer) Close(ctx qra.Identity) error {
 
+	userID := ctx.Me()
+	log.Printf("Close : userID [%s]", userID)
+
+	var sessionID string
+	err := ctx.Session(&sessionID)
+	if err != nil {
+		log.Printf("Close : get session : err [%s]", err)
+		return err
+	}
+	log.Printf("Close : get session : sID [%s]", sessionID)
+
+	var res string
+	err = s.DB.Get(&res, `
+		DELETE FROM session WHERE token=$1 RETURNING 'OK';
+	`, sessionID)
+	if err != nil {
+		log.Printf("Close : remove session : err [%s]", err)
+	}
+
+	return err
+}
+
+// Designationer struct
+type Designationer struct {
+	DB *pgwp.Pool
+}
+
+// Design type from database.
+type Design struct {
+	ID              string `db:"id"`
+	Identity        string `db:"identity"`
+	Permission      []byte `db:"permission"`
+	Resource        []byte `db:"resource"`
+	IssuerID        string `db:"issuer_id"`
+	IssuerSignature string `db:"issuer_signature"`
+	ExpiresAt       string `db:"expires_at"`
+}
+
+// Search method searchs permission-designation relations for
+// Identity.
+//
+// It doesn't validate permission signature because it would
+// be CPU expensive. Every permission in database is view
+// AS secure because Allow and Revoke methods do all the RSA
+// operations.
+func (p *Designationer) Search(ctx qra.Identity, writer io.Writer, filter string) error {
+
+	me := ctx.Me()
+	log.Printf("Search : me [%v] filter [%v]", me, filter)
+
+	x := strings.Split(filter, ":")
+	if len(x) < 2 {
+		return errResourceNotSpecified
+	}
+	permission := x[0]
+	resource := x[1]
+
+	var des Design
+	err := p.DB.Get(&des, `
+		SELECT permission,resource FROM designation
+		WHERE identity=$1 AND permission=$2 AND resource=$3;
+	`, me, permission, resource)
+	if err != nil {
+		log.Printf("Search : find designation: err [%s]", err)
+	}
+
+	return err
+}
+
+// NPE struct stands for Non-Person Entity.
+type NPE struct {
+	ID         string `db:"id"`
+	Name       string `db:"name"`
+	PrivateKey []byte `db:"private_key"`
+	PublicKey  []byte `db:"public_key"`
+}
+
+// Allow method share permission from ctx to dst.
+//
+// Every allow operation is signed using RSA 4096.
+func (p *Designationer) Allow(ctx qra.Identity, permission, resource, dst string, expires time.Time) error {
+	me := ctx.Me()
+	log.Printf("Allow : identity [%v]", me)
+
+	// locate identity and session.
+
+	var token string
+	err := ctx.Session(&token)
+	if err != nil {
+		return err
+	}
+
+	var sessionID string
+	err = p.DB.Get(&sessionID, `
+		SELECT token FROM session
+		WHERE token=$1;
+	`, token)
+	if err != nil {
+		log.Printf("Allow : locate session : err [%s]", err)
+	}
+
+	// locate identity permission.
+
+	var sID string
+	err = p.DB.Get(&sID, `
+		SELECT token FROM session
+		WHERE token=$1;
+	`, token)
+	if err != nil {
+		log.Printf("Allow : locate session : err [%s]", err)
+	}
+
+	// share permission with dst using RSA encription.
+
+	var des Design
+	err = p.DB.Get(&des, `
+		SELECT
+			identity,
+			permission,
+			resource,
+			issuer_id,
+			issuer_signature,
+			expires_at
+		FROM designation
+		WHERE identity=$1 AND permission=$2 AND resource=$3;
+	`, me, permission, resource)
+	if err != nil {
+		log.Printf("Allow : locate designation : err [%s]", err)
+		return err
+	}
+
+	var npe NPE
+	err = p.DB.Get(&des, `
+		SELECT
+			id,
+			name,
+			private_key,
+			public_key
+		FROM identity
+		WHERE name=$1;
+	`, me)
+	if err != nil {
+		log.Printf("Allow : locate identity : err [%s]", err)
+		return err
+	}
+	if len(npe.PrivateKey) < 1 || len(npe.PublicKey) < 1 {
+		return errPermissionNotFound
+	}
+
+	return errPermissionNotFound
+}
+
+// Revoke method revokes a permission that Identity give to dst.
+//
+// Every revoke operation verifies signed RSA keys encription.
+func (p *Designationer) Revoke(ctx qra.Identity, permission, dst string) error {
+
+	userID := ctx.Me()
+	log.Printf("Revoke : userID [%v]", userID)
+
+	return nil
 }
