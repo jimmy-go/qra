eea60900eadc2030f98210579f27e3ae1208114d
Angel Del Castillo <jubu201504@gmail.com>
2017-02-02 15:32:51 -0600

    pgmanager: change table names

--- pgmanager/migration/0001_initial.down.sql
+++ pgmanager/migration/0001_initial.down.sql
@@ -1,10 +1,10 @@
 /*
     PostgreSQL default migration for QRA manager.
 */
-DROP TABLE IF EXISTS session;
-DROP TABLE IF EXISTS trail;
-DROP TABLE IF EXISTS designation;
-DROP TABLE IF EXISTS identity;
+DROP TABLE IF EXISTS qra_session;
+DROP TABLE IF EXISTS qra_trail;
+DROP TABLE IF EXISTS qra_designation;
+DROP TABLE IF EXISTS qra_identity;
 DROP TYPE IF EXISTS perm;
 DROP TYPE IF EXISTS status;
 --DROP FUNCTION update_modified_column();
